3c8585fb71cf907c2bec0e85197e797a1e992b22
toba717 <toba7171@gmail.com>
2022-06-27 14:44:21 -1000

    Initial Commit

---
+++ Project1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Project1
+//
+//  Created by Takao Oba on 6/27/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
